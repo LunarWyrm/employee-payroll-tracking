65f6c6758a3e2c0a5ae9d2900614eb023c9f474b
LunarWyrm <englerprogramming@gmail.com>
2024-04-19 00:46:18 -0400

    Added starter code and custom code
    I had to add copy and paste both here because I could not figure out how to do this through the curly-potato repository and did not want to lose my progress.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # employee-payroll-tracking
  Tracks the payroll of employees.
+
+
+Used as reference:
+
+Lines 9 through 11 in script.js are from working with other student in class.
+Lines 27 through 33 in script.js were made referencing https://stackoverflow.com/questions/29544371/finding-the-average-of-an-array-using-js
+
---
+++ script.js
@@ -0,0 +1,108 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+
+// Collect employee data
+const collectEmployees = function() {
+  // TODO: Get user input to create and return an array of employee objects
+  let employees = []
+  while(true) {
+    let firstName = prompt("Employee's First Name: ");
+    if(!firstName) {
+      break;
+    }
+    console.log()
+    let lastName = prompt("Employee's Last Name: ");
+    console.log(lastName)
+    let salary = prompt("Employee's Salary: ");
+    if(isNaN(salary)) {
+      alert("Please enter a number for the salary.");
+    }
+    console.log(employees)
+}
+}
+
+// Display the average salary
+const displayAverageSalary = function(employeesArray) {
+  // TODO: Calculate and display the average salary
+  let averageSalarySubtotal = 0;
+  
+  for (var i = 0; i <employeesArray.length; i++) {
+  averageSalarySubtotal += employeesArray[i];
+  }
+  let averageSalary = averageSalarySubtotal / employeesArray.length;
+  console.log(averageSalary)
+
+  console.log($`AVG Salary {averageSalary}`)
+}
+
+// Select a random employee
+const getRandomEmployee = function(employeesArray) {
+  // TODO: Select and display a random employee
+  Math.random()
+}
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function(employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+      style:"currency",
+      currency:"USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function() {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  employees.sort(function(a,b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
